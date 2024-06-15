29571d3b9f53185bb478af0022b32c49ae4aa973
SSD2111 <162426621+SSD2111@users.noreply.github.com>
2024-06-15 17:04:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+var pages = document.getElementsByClassName('page');
+var forward = new Audio('./sounds/forward.wav')
+var backward = new Audio('./sounds/backward.wav')
+
+var song1 = new Audio('./songs/birthday-song.mp3')
+
+for (var i = 0; i < pages.length; i++) {
+    var page = pages[i];
+    if (i % 2 === 0) {
+        page.style.zIndex = (pages.length - i);
+    }
+}
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    song1.play()    
+    for (var i = 0; i < pages.length; i++) {
+        // var page = pages[i];
+        pages[i].pageNum = i + 1;
+        pages[i].onclick = function () {
+            if (this.pageNum % 2 === 0) {
+                backward.play();
+                this.classList.remove('flipped');
+                this.previousElementSibling.classList.remove('flipped');
+            }
+            else {
+                forward.play();
+                this.classList.add('flipped');
+                this.nextElementSibling.classList.add('flipped');
+            }
+        }
+    }
+})
+
